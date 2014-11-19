352c63136fb003d6de6dc257e61594aef069da68
Bryce Kahle <bryce.kahle@mlssoccer.com>
2014-11-19 15:50:23 -0500

    add option for security group to elb presence

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/brycekahle/goamz/ec2"
 	"github.com/crowdmob/goamz/aws"
 	"github.com/crowdmob/goamz/elb"
 	"log"
@@ -12,10 +13,12 @@ import (
 	"time"
 )
 
-var lbname, region, accesskey, secretkey string
+var lbname, region, accesskey, secretkey, securityGroupID string
+var awsec2 *ec2.EC2
 
 func init() {
 	flag.StringVar(&lbname, "lbname", os.Getenv("ELB_NAME"), "name of the ELB to register the instance with")
+	flag.StringVar(&securityGroupID, "groupid", os.Getenv("SECURITY_GROUP_ID"), "id of the EC2 security group to add this instance to")
 	flag.StringVar(&region, "region", os.Getenv("AWS_REGION"), "AWS region in which the ELB resides")
 	flag.StringVar(&accesskey, "accesskey", os.Getenv("AWS_ACCESS_KEY"), "AWS Access Key")
 	flag.StringVar(&secretkey, "secretkey", os.Getenv("AWS_SECRET_KEY"), "AWS Secret Key")
@@ -39,6 +42,25 @@ func main() {
 		log.Fatalln("Unable to get AWS auth", err)
 	}
 
+	if securityGroupID != "" {
+		awsec2 = ec2.New(auth, aws.GetRegion(region))
+
+		groupMap := getSecurityGroupIds(instanceID)
+		groupMap[securityGroupID] = true
+		groupIds := make([]string, 0, len(groupMap))
+		for id := range groupMap {
+			groupIds = append(groupIds, id)
+		}
+
+		opts := &ec2.ModifyInstanceAttributeOptions{SecurityGroups: ec2.SecurityGroupIds(groupIds...)}
+		resp, err := awsec2.ModifyInstanceAttribute(instanceID, opts)
+		if err != nil || !resp.Return {
+			log.Fatalln("Error adding security group to instance", err)
+		}
+
+		log.Printf("Added security group %s to instance %s\n", securityGroupID, instanceID)
+	}
+
 	awselb := elb.New(auth, aws.GetRegion(region))
 	_, err = awselb.RegisterInstancesWithLoadBalancer([]string{instanceID}, lbname)
 	if err != nil {
@@ -59,4 +81,34 @@ func main() {
 	}
 
 	log.Printf("Deregistered instance %s with elb %s\n", instanceID, lbname)
+
+	if securityGroupID != "" {
+		groupMap := getSecurityGroupIds(instanceID)
+		delete(groupMap, securityGroupID)
+		groupIds := make([]string, 0, len(groupMap))
+		for id := range groupMap {
+			groupIds = append(groupIds, id)
+		}
+
+		opts := &ec2.ModifyInstanceAttributeOptions{SecurityGroups: ec2.SecurityGroupIds(groupIds...)}
+		resp, err := awsec2.ModifyInstanceAttribute(instanceID, opts)
+		if err != nil || !resp.Return {
+			log.Fatalln("Error removing security group from instance", err)
+		}
+
+		log.Printf("Removed security group %s from instance %s\n", securityGroupID, instanceID)
+	}
+}
+
+func getSecurityGroupIds(instanceID string) map[string]bool {
+	resp, err := awsec2.DescribeInstanceAttribute(instanceID, "groupSet")
+	if err != nil {
+		log.Fatalln("Error describing instance attributes", err)
+	}
+
+	groupIds := make(map[string]bool)
+	for _, g := range resp.SecurityGroups {
+		groupIds[g.Id] = true
+	}
+	return groupIds
 }
