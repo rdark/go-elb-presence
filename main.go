a6b2b85ee18c93d2897a3679b0adcb10c2f3bb86
Richard Clark <richard@fohnet.co.uk>
2015-07-16 13:45:47 +0100

    allow instance ID to be passed as an argument

--- main.go
+++ main.go
@@ -31,12 +31,13 @@ func (i *csv) Set(value string) error {
 	return nil
 }
 
-var region, accesskey, secretkey, securityGroupID string
+var region, accesskey, secretkey, securityGroupID, instanceID string
 var lbnames csv
 var awsec2 *ec2.EC2
 
 func init() {
-	flag.Var(&lbnames, "lbname", "name of the ELB to register the instance with")
+	flag.Var(&lbnames, "lbnames", "name(s) of the ELB to register the instance with (use CSV string for multiple)")
+	flag.StringVar(&instanceID, "instanceId", os.Getenv("INSTANCE_ID"), "ID of the EC2 ec2 instance - will be pulled from metadata API if not given")
 	flag.StringVar(&securityGroupID, "groupid", os.Getenv("SECURITY_GROUP_ID"), "id of the EC2 security group to add this instance to")
 	flag.StringVar(&region, "region", os.Getenv("AWS_REGION"), "AWS region in which the ELB resides")
 	flag.StringVar(&accesskey, "accesskey", os.Getenv("AWS_ACCESS_KEY"), "AWS Access Key")
@@ -54,10 +55,16 @@ func init() {
 }
 
 func main() {
-	instanceID := aws.InstanceId()
-	if instanceID == "unknown" {
-		log.Fatalln("Unable to get instance id")
-	}
+    var inst_id string
+
+    if instanceID == "" {
+	    inst_id := aws.InstanceId()
+        if inst_id == "unknown" {
+            log.Fatalln("Unable to get instance id")
+        }
+    } else {
+        inst_id = instanceID
+    }
 
 	auth, err := aws.GetAuth(accesskey, secretkey, "", time.Time{})
 	if err != nil {
@@ -67,7 +74,7 @@ func main() {
 	if securityGroupID != "" {
 		awsec2 = ec2.New(auth, aws.GetRegion(region))
 
-		groupMap := getSecurityGroupIds(instanceID)
+		groupMap := getSecurityGroupIds(inst_id)
 		groupMap[securityGroupID] = true
 		groupIds := make([]string, 0, len(groupMap))
 		for id := range groupMap {
@@ -75,22 +82,22 @@ func main() {
 		}
 
 		opts := &ec2.ModifyInstanceAttributeOptions{SecurityGroups: ec2.SecurityGroupIds(groupIds...)}
-		resp, err := awsec2.ModifyInstanceAttribute(instanceID, opts)
+		resp, err := awsec2.ModifyInstanceAttribute(inst_id, opts)
 		if err != nil || !resp.Return {
 			log.Fatalln("Error adding security group to instance", err)
 		}
 
-		log.Printf("Added security group %s to instance %s\n", securityGroupID, instanceID)
+		log.Printf("Added security group %s to instance %s\n", securityGroupID, inst_id)
 	}
 
 	awselb := elb.New(auth, aws.GetRegion(region))
 	for _, lbname := range lbnames {
-		_, err = awselb.RegisterInstancesWithLoadBalancer([]string{instanceID}, lbname)
+		_, err = awselb.RegisterInstancesWithLoadBalancer([]string{inst_id}, lbname)
 		if err != nil {
 			log.Fatalln("Error registering instance", err)
 		}
 
-		log.Printf("Registered instance %s with elb %s\n", instanceID, lbname)
+		log.Printf("Registered instance %s with elb %s\n", inst_id, lbname)
 	}
 
 	c := make(chan os.Signal, 1)
@@ -100,16 +107,16 @@ func main() {
 	<-c
 
 	for _, lbname := range lbnames {
-		_, err = awselb.DeregisterInstancesFromLoadBalancer([]string{instanceID}, lbname)
+		_, err = awselb.DeregisterInstancesFromLoadBalancer([]string{inst_id}, lbname)
 		if err != nil {
 			log.Fatalln("Error deregistering instance", err)
 		}
 
-		log.Printf("Deregistered instance %s with elb %s\n", instanceID, lbname)
+		log.Printf("Deregistered instance %s with elb %s\n", inst_id, lbname)
 	}
 
 	if securityGroupID != "" {
-		groupMap := getSecurityGroupIds(instanceID)
+		groupMap := getSecurityGroupIds(inst_id)
 		delete(groupMap, securityGroupID)
 		groupIds := make([]string, 0, len(groupMap))
 		for id := range groupMap {
@@ -117,12 +124,12 @@ func main() {
 		}
 
 		opts := &ec2.ModifyInstanceAttributeOptions{SecurityGroups: ec2.SecurityGroupIds(groupIds...)}
-		resp, err := awsec2.ModifyInstanceAttribute(instanceID, opts)
+		resp, err := awsec2.ModifyInstanceAttribute(inst_id, opts)
 		if err != nil || !resp.Return {
 			log.Fatalln("Error removing security group from instance", err)
 		}
 
-		log.Printf("Removed security group %s from instance %s\n", securityGroupID, instanceID)
+		log.Printf("Removed security group %s from instance %s\n", securityGroupID, inst_id)
 	}
 }
 
